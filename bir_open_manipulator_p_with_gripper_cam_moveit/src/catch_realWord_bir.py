6bbfdbed29347ea0ee14bac064385d3563406f81
Kaike Wesley Reis <kaikewesley@hotmail.com>
2020-01-29 18:07:50 -0300

    changes

--- bir_open_manipulator_p_with_gripper_cam_moveit/src/catch_realWord_bir.py
+++ bir_open_manipulator_p_with_gripper_cam_moveit/src/catch_realWord_bir.py
@@ -20,19 +20,20 @@ END_CONDITION = 0.20                            # END CONDITION FOR SEARCHING
 SEQ_FRAMES = 15                                # HOW MANY FRAME DO YOU NEED TO COUNT TO FIND THE DESIRED TAG
 JOINTS_TOLERANCE = 0.05
 
-class openManipulatorBIR:
+class openManipulatorPRO:
     def __init__(self):
         # ROS NODE INIT
         rospy.init_node('apriltag_detector_go2goal_gripper', anonymous=True)        
-        # MOVEIT INIT
+        # MOVEIT - INIT
         moveit_commander.roscpp_initialize(sys.argv)
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
-        self.group_name = "arm"
-        self.group = moveit_commander.MoveGroupCommander(self.group_name)
+        self.group = moveit_commander.MoveGroupCommander("arm")
         self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory,queue_size=20)
-        # TAG DETECTION INIT
-        self.tag_id_subscriber = rospy.Subscriber('/tag_detections', AprilTagDetectionArray,self.callback)
+        # MOVEIT - PARAMETERS
+        self.group.set_goal_position_tolerance(JOINTS_TOLERANCE)            # GOAL TOLERANCE
+        self.group.set_max_velocity_scaling_factor(0.95)                    # MAX SPEED FACTOR
+        self.group.set_planning_time(5)                                     # TIME TO PLANNING
         # GRIPPER SERVICE INIT
         rospy.wait_for_service('/open_manipulator_pro/goal_tool_control') 
         self.gripper_commander = rospy.ServiceProxy('/open_manipulator_pro/goal_tool_control',SetJointPosition)   
@@ -41,11 +42,9 @@ class openManipulatorBIR:
         self.gripper_msg.max_accelerations_scaling_factor = 0.1
         self.gripper_msg.max_velocity_scaling_factor = 0.5
         self.gripper_msg.position = [2]                                   # START OPEN                                                    
-        # DEFINE MOVEIT PARAMETERS
-        self.group.set_goal_position_tolerance(JOINTS_TOLERANCE)          # GOAL TOLERANCE
-        self.group.set_max_velocity_scaling_factor(0.1)                   # MAX SPEED FACTOR
-        self.group.set_planning_time(5)                                   # TIME TO PLANNING
-        # DEFINE TAG SEARCH AUX
+        # TAG DETECTION - INIT
+        self.tag_id_subscriber = rospy.Subscriber('/tag_detections', AprilTagDetectionArray,self.callback)
+        # TAG DETECTION - VARS
         self.id_found = 0                                    # FOUND TAG
         self.seq_counts = 0                                  # SEQ COUNTS THAT THE RBG GET THE FRAME
         self.init_pose = 0                                   # STOP FIND TAG BEFORE START ROUTINE
@@ -72,9 +71,8 @@ class openManipulatorBIR:
                 self.group.clear_pose_targets()
                 rospy.sleep(1)
                 break
-    
-    # FUNCTION - MOVE JOINT
-    # FUNCTION - GO TO POSE WITH SECURITY
+
+    # FUNCTION - MOVE JOINT WITH SECURITY
     def move_joint_security_sequential_execution(self, jointID, jointValueDegree):
         # FIX PARAMETERS
         jointID = jointID - 1
@@ -98,14 +96,14 @@ class openManipulatorBIR:
 
     # FUNCTION - ROTATE JOINT 1 LOOKING FOR A TAG
     def search_tag(self):
-        # 1 - DEFINE A VARIABLE FORMAT EQUIVALENT TO JOINTS
+        # DEFINE A VARIABLE FORMAT EQUIVALENT TO JOINTS
         joint_goal = self.group.get_current_joint_values()
-        # 2 - DECISION :: FINISH THE NODE IF JOINT ANGLE IS CLOSE TO PATH END
+        # DECISION :: FINISH THE NODE IF JOINT ANGLE IS CLOSE TO PATH END
         if abs(abs(joint_goal[0]) - abs(LIMIT_J1)) < END_CONDITION:
             # 2.1 - BEFORE END NODE, SEND TO pSearch
             rospy.loginfo('############# GO TO HOME POSE, FINISH ROUTINE.')
             self.security_sequential_execution2('pHome')
-            rospy.sleep(10)
+            rospy.sleep(5)
             sys.exit()
         # 3 - INSERT DESIRED ANGLE
         joint_goal[0] = joint_goal[0] + ROTATE_DEGREE
@@ -139,7 +137,7 @@ class openManipulatorBIR:
     # FUNCTION - COMMAND GRIPPER
     def go_gripper(self, positionValue):
         self.gripper_msg.position = [positionValue]
-        self.gripper_commander('arm',self.gripper_msg,5.0)
+        self.gripper_commander('arm', self.gripper_msg, 5.0)
             
     # FUNCTION - PROCESS ROUTINE
     def process(self):
@@ -194,9 +192,8 @@ class openManipulatorBIR:
 if __name__ == '__main__':
     try:
         # INITIALIZE YOUR OBJECT
-        omanip = openManipulatorBIR()
+        omanip = openManipulatorPRO()
         # INITIALIZE THE OPERATION BY CLICKING ENTER
-        raw_input("Press Enter to start!")
         # OPERATION
         omanip.process2()  
 
